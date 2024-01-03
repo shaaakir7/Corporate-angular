4c1a982459d89919fb1c902eda5f4636aedf10fd
Gopz12 <gopika1210001@gmail.com>
2024-01-03 12:33:52 +0530

    modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,4 +25,4 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
