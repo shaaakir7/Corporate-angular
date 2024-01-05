be67b133ca3514ca1f2cc5c68fb1d3bf7f669317
shaaakir7 <ahamedshakir222@gmail.com>
2024-01-05 11:25:09 +0530

    shakir ,added loader function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'corporate-con';
-
-
   isMenuOpen = false;
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
+
+  // onload function for loader
+  ngOnInit(): void {
+    this.renderer.listen('window', 'load', () => {
+      setTimeout(() => {
+        this.renderer.setStyle(this.el.nativeElement.querySelector('#loaderWrap'), 'display', 'none');
+        this.renderer.setStyle(this.el.nativeElement.querySelector('#pageContent'), 'display', 'block');
+        console.log('loaded');
+      }, 2500);
+    });
+  }
 }
