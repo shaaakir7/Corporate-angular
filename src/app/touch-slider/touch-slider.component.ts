36185b2548f9b3046ab4d9c1a3bf0a8a74834e98
srohit192002 <srohitk192002@gmail.com>
2024-01-07 15:24:34 +0530

    "Modified and add slide card"

--- src/app/touch-slider/touch-slider.component.ts
+++ src/app/touch-slider/touch-slider.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import 'hammerjs';
 export interface Slide {
   imgSrc:string;
   imgAlt:string;
